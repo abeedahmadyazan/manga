68aa4bd1b199ec86fa6d9a375d2c98272ba6ef87
abeedahmadyazan <abeedahmadyazan@users.noreply.github.com>
2026-07-08 03:13:05 +0000

    fix: add missing Toast import in AdminPanelActivity

--- app/src/main/java/com/yazan/manga/AdminPanelActivity.kt
+++ app/src/main/java/com/yazan/manga/AdminPanelActivity.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
