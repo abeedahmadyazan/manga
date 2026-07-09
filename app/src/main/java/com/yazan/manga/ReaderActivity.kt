bc569bc4efece025a48b1b3be6ad0696b0f78a6d
abeedahmadyazan <89158055+abeedahmadyazan@users.noreply.github.com>
2026-07-09 14:06:59 +0300

    fix: all Glide RequestListener params non-nullable (matches Java API: Target<Drawable!>!, DataSource, Any)

--- app/src/main/java/com/yazan/manga/ReaderActivity.kt
+++ app/src/main/java/com/yazan/manga/ReaderActivity.kt
@@ -235,10 +235,10 @@ class ReaderActivity : AppCompatActivity() {
         // Use Glide's RequestListener with platform-typed params (matching Java API exactly).
         val listener = object : com.bumptech.glide.request.RequestListener<android.graphics.drawable.Drawable> {
             override fun onResourceReady(
-                resource: android.graphics.drawable.Drawable?,
-                model: Any?,
-                target: com.bumptech.glide.request.target.Target<android.graphics.drawable.Drawable>?,
-                dataSource: com.bumptech.glide.load.DataSource?,
+                resource: android.graphics.drawable.Drawable,
+                model: Any,
+                target: com.bumptech.glide.request.target.Target<android.graphics.drawable.Drawable>,
+                dataSource: com.bumptech.glide.load.DataSource,
                 isFirstResource: Boolean
             ): Boolean {
                 loadingIndicator.visibility = View.GONE
@@ -247,8 +247,8 @@ class ReaderActivity : AppCompatActivity() {
 
             override fun onLoadFailed(
                 e: com.bumptech.glide.load.engine.GlideException?,
-                model: Any?,
-                target: com.bumptech.glide.request.target.Target<android.graphics.drawable.Drawable>?,
+                model: Any,
+                target: com.bumptech.glide.request.target.Target<android.graphics.drawable.Drawable>,
                 isFirstResource: Boolean
             ): Boolean {
                 loadingIndicator.visibility = View.GONE
